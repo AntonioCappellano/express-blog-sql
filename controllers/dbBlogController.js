025641a76030493fe6c10f8169ce9c46f0dbd8ad
AntonioCappellano <antoniocappellano97@gmail.com>
2026-03-14 15:24:01 +0100

    feat: implement index logic

---
+++ controllers/dbBlogController.js
@@ -0,0 +1,28 @@
+const connection = require("../data/db");
+function index(req, res) {
+  const sql = `SELECT * FROM posts`;
+
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: `Database query failed` });
+    res.json({ success: true, message: `Post nel DB`, results });
+  });
+}
+
+function show(req, res) {}
+
+function store(req, res) {}
+
+function update(req, res) {}
+
+function modify(req, res) {}
+
+function destroy(req, res) {}
+
+module.exports = {
+  index,
+  show,
+  store,
+  update,
+  modify,
+  destroy,
+};
